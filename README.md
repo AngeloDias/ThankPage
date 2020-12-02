660293976a4f6f9811b3087caba9ac067964ce83
angelo.santana <angelossdias@gmail.com>
2020-12-02 14:38:49 -0300

    feat: adding more section (check margins)

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 <img src="prints/pagePart1.PNG">
 <img src="prints/pagePart2.PNG">
 <img src="prints/pagePart3.PNG">
+
+<!-- ####### References ####### -->
+
+<!-- https://masakudamatsu.medium.com/draw-horizontal-lines-on-a-webpage-891cd93b8178 -->
+
+<!-- ####### END ####### -->
--- src/components/OrderConfirmationComponent.js
+++ src/components/OrderConfirmationComponent.js
@@ -28,7 +28,7 @@ export default function OrderConfirmationComponent() {
 
                         <div className="col s12 m6 l6">
                             <div className="icon-block">
-                                <img src={malinois} alt="dog" width="300" className="materialboxed" />
+                                <img src={malinois} alt="dog" width="250" className="materialboxed" />
                             </div>
                         </div>
 
@@ -37,6 +37,47 @@ export default function OrderConfirmationComponent() {
                                 <h5 className="center">Malinois shepherd</h5>
 
                                 <p className="light">The Belgian Malinois (pronounced MAL-in-wah) is a medium-size Belgian shepherd dog that at first glance resembles a German Shepherd Dog. Malinois are shorthaired, fawn-colored dogs with a black mask. They are one of four types of Belgian herding dogs, and have been shown in the U.S. as a separate breed since 1959.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="line-horizontal-container">
+                    <div className="line-horizontal"></div>
+                </div>
+
+                <div className="section">
+                    <div className="row center">
+                        <table className="responsive-table centered" height="101" border="0" role="presentation">
+                            <tbody>
+                                <tr>
+                                    <td><strong>Height:</strong></td>
+                                    <td><strong>Weight:</strong></td>
+                                    <td><strong>Life Span:</strong></td>
+                                </tr>
+                                <tr>
+                                    <td>22 to 26 inches tall at the shoulder</td>
+                                    <td>40 to 80 pounds</td>
+                                    <td>12 to 14 years</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div className="section order-shipping-info">
+                    <div className="row">
+                        <h3 className="center">Order & shipping info</h3>
+
+                        <div className="col s12 m6 l6">
+                            <div className="icon-block">
+                                <h5 className="center">Dog details</h5>
+                            </div>
+                        </div>
+
+                        <div className="col s12 m6 l6">
+                            <div className="icon-block">
+                                <h5 className="center">Shipping address</h5>
                             </div>
                         </div>
                     </div>
