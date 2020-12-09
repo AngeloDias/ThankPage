d9718d6f8de49373fcb5681140646579557e75fd
angelo.santana <angelossdias@gmail.com>
2020-12-09 08:51:38 -0300

    feat: changing the link redirect type to better use SEO

--- src/components/PageBottomComponent.js
+++ src/components/PageBottomComponent.js
@@ -12,11 +12,11 @@ export default function PageBottomComponent() {
             <div className="footer-copyright">
                 <div className="container">
                     <div className="container grey-text">
-                        Made by <a target="_blank" href="https://www.instagram.com/angelo.d.san/">@angelo.d.san</a> using <a className="grey-text text-lighten-3" target="_blank" href="http://materializecss.com">Materialize CSS</a>
+                        Made by <a href="https://www.instagram.com/angelo.d.san/">@angelo.d.san</a> using <a className="grey-text text-lighten-3" target="_blank" rel="noreferrer" href="http://materializecss.com">Materialize CSS</a>
                     </div>
 
                     <div>
-                        <p className="grey-text">Page used as reference: <a target="_blank" href="https://viewstripo.email/template/d34a7d1c-6362-4200-b484-4f0cb2b8b1c6">Order confirmation template</a>.</p>
+                        <p className="grey-text">Page used as reference: <a target="_blank" rel="noreferrer" href="https://viewstripo.email/template/d34a7d1c-6362-4200-b484-4f0cb2b8b1c6">Order confirmation template</a>.</p>
                     </div>
                 </div>
             </div>
